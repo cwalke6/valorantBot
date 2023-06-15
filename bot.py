564c674e37d873f28bc56cbaba42a4fc001abaff
M4nchy <42629584+M4nchy@users.noreply.github.com>
2023-06-14 20:26:08 -0400

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,44 @@
+import discord
+import responses
+
+
+
+async def send_message(message, user_message, is_private):
+    try:
+        response = responses.get_response(user_message)
+        await message.author.send(response) if is_private else await message.channel.send(response) # This format is fucking retarded but im following the tutorial
+
+    except Exception as e:
+        print(e)
+
+
+
+def run_discord_bot():
+    TOKEN = 'MTExODM2MTM0NjEyMDA5NzgzMw.GQnzDh.5rjjdmEVdnrQqSXKfBUrxBXM-5aSH06jN4ydEI'
+    intents = discord.Intents.default() # Look for this as the first thing we might have to change for integration if we find problems
+    intents.message_content = True
+    client = discord.Client(intents=intents)
+
+    @client.event
+    async def on_ready():
+        print(f'{client.user} is now running!')
+
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user: # to stop infinite loop from bot responding to itself
+            return
+        
+        username = str(message.author) # stores username
+        user_message = str(message.content)
+        channel = str(message.channel)
+
+        print(f'{username} said: "{user_message}" ({channel})')
+
+        if user_message[0] == '?': # private message for a command and just want to get rid of ? which is done in next line.
+            user_message = user_message[1:]
+            await send_message(message, user_message, is_private=True)
+        else:
+            await send_message(message, user_message, is_private=False)
+
+    client.run(TOKEN)
