3ffd692dd6ec129896b14275493160a05b9b7108
Christian Walker <walkercd21@gmail.com>
2023-08-15 18:04:18 -0400

    Added another command mainly to test git is set up correctly

--- responses.py
+++ responses.py
@@ -12,8 +12,12 @@ def get_response(message: str) -> str:
     
     if p_message == '!help':
         return '`Here is a link to the github page with a readme on how to use this bot: https://github.com/M4nchy/valorantBot`'
+    
     if p_message == '!login':
         return 'login command'
+
+    if p_message == '!stats':
+        return 'stats command'
     
 
 # If message not matched to above case will respond to every message
