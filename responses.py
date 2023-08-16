7e4219a70c5183479b79edd5f12386b579d864d7
Christian Walker <walkercd21@gmail.com>
2023-08-15 21:32:20 -0400

    Added a sqlite database and !register command to let people register their valorant usernames and tags in the data base. Need to figure out how to assosciate a discord user with that specific username and tag. A way to do this is adding another input to the data base like a discord user ID key to assosciate and check the userID whenever they use the message and then return that certain username and tag. Check discord for more details

--- responses.py
+++ responses.py
@@ -1,4 +1,5 @@
 import random
+import sqlite3
 
 def get_response(message: str) -> str:
     # python case sensitive so important to have this
@@ -13,7 +14,21 @@ def get_response(message: str) -> str:
     if p_message == '!help':
         return '`Here is a link to the github page with a readme on how to use this bot: https://github.com/M4nchy/valorantBot`'
     
+    if p_message[:9] == '!register':
+        usernameAndTag = p_message[10:].partition('#')
+        username = usernameAndTag[0]
+        tag = usernameAndTag[2]
+        
+        sqliteConnection = sqlite3.connect('usernames.db')
+        cursor = sqliteConnection.cursor()
+        sqlite_insert_query = "INSERT INTO usernames VALUES (\'"+username+"\',\'"+tag+"\')"
+        count = cursor.execute(sqlite_insert_query)
+        
+        sqliteConnection.commit()
+        print("Row inserted successfully ", cursor.rowcount)
+        cursor.close()
+        
+        return 'Username:' + username + 'tag:' + tag
 
 # If message not matched to above case will respond to every message
-# Maybe look for a better way to implement this not just "return None" since that gives an error in terminal.
-    return None 
+    return "Unrecognized Command"
